cb30616b8179addd49cff1962903d8799bfa6caf
noah <noahitall2002@gmail.com>
2022-06-30 16:40:22 -0400

    made smol and changed function loop behavior

--- Menu.py
+++ Menu.py
@@ -1,11 +1,11 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((600, 700))
 screen.fill((2, 0, 115))
 
 class Button:
-    def __init__(self, pos, text, event):
+    def __init__(self, pos, text):
         self.x, self.y = pos
         self.font = pygame.font.SysFont("Calibri", 20)
         self.text = self.font.render(text, 1, (0, 0, 0))
@@ -14,7 +14,7 @@ class Button:
         self.surface.fill(pygame.Color('grey27'))
         self.surface.blit(self.text, ((150-self.size[0])/2, (40-self.size[1])/2))
         self.rect = pygame.Rect(self.x, self.y, 150, 40)
-        self.event = event
+        self.triggered = 0
 
     def show(self):
         screen.blit(self.surface, (self.x, self.y))
@@ -23,14 +23,14 @@ class Button:
         mousePos = pygame.mouse.get_pos()
         if self.rect.collidepoint(mousePos):
             if pygame.mouse.get_pressed(num_buttons=3)[0]:
-                self.event()
+                self.triggered = 1
 
 
-def mainloop():
-    PvP = Button((275, 800), "Player vs Player", localPvP)
-    OPvP = Button((275, 900), "Online PvP", onlinePvP)
-    PvAI = Button((575, 800), "Player vs AI", localPvAI)
-    AIvAI = Button((575, 900), "AI vs AI", localAIvAI)
+def menu():
+    PvP = Button((125, 500), "Player vs Player")
+    OPvP = Button((125, 600), "Online PvP")
+    PvAI = Button((325, 500), "Player vs AI")
+    AIvAI = Button((325, 600), "AI vs AI")
     logo = pygame.image.load('logo2.png')
     while True:
         for event in pygame.event.get():
@@ -44,7 +44,17 @@ def mainloop():
         OPvP.show()
         PvAI.show()
         AIvAI.show()
-        screen.blit(logo, (0, 200))
+
+        if PvP.triggered:
+            return 1
+        elif OPvP.triggered:
+            return 2
+        elif PvAI.triggered:
+            return 3
+        elif AIvAI.triggered:
+            return 4
+        #screen.blit(logo, (0, 200))
+
         pygame.display.update()
 
 
